9e9bfbea228554f495aae9e370c9c2d9607fc3d1
adrsh-prthpp <pm.adharsh@gmail.com>
2026-04-18 23:03:30 -0700

    fixed vercel error

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import { notFound } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Tag } from "@/components/ui/tag";
